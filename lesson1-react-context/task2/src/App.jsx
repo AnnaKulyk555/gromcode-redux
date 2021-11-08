faf0ba9b29fd64fdeadbd2b1485386cf7d300967
annaKulyk <ankakulik1998@gmail.com>
2021-11-08 23:32:04 +0200

    User menu with React Context

---
+++ lesson1-react-context/task2/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { UserDataContext } from './userData-context';
+
+import Header from './Header';
+
+class App extends React.Component {
+  state = {
+    userData: { name: 'Nikola Tesla', avatar_url: 'https://avatars3.githubusercontent.com/u10001' },
+  };
+
+  render() {
+    return (
+      <div className="page">
+        <UserDataContext.Provider value={this.state.userData}>
+          <Header />
+        </UserDataContext.Provider>
+      </div>
+    );
+  }
+}
+
+export default App;
