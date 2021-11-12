31600479f5df48e6aecca0b2b23ec24d97bb6252
annaKulyk <ankakulik1998@gmail.com>
2021-11-12 14:45:39 +0200

    redux-dev-tools

--- lesson3-combine-reducers/task2/src/counter.js
+++ lesson3-combine-reducers/task2/src/counter.js
@@ -2,7 +2,6 @@ import { createStore } from 'redux';
 
 export const INCREMENT = 'COUNTER/INCREMENT';
 export const DECREMENT = 'COUNTER/DECREMENT';
-export const RESET = 'COUNTER/RESET';
 
 export const increment = () => ({
   type: INCREMENT,
@@ -12,18 +11,12 @@ export const decrement = () => ({
   type: DECREMENT,
 });
 
-export const reset = () => ({
-  type: RESET,
-});
-
 const counterReducer = (state = 0, action) => {
   switch (action.type) {
     case INCREMENT:
       return state + 1;
     case DECREMENT:
       return state - 1;
-    case RESET:
-      return 0;
     default:
       return state;
   }
