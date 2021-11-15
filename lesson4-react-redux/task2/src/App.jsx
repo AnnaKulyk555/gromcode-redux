56a7936fa46b90ad964058e16fcc01042593030b
annaKulyk <ankakulik1998@gmail.com>
2021-11-15 22:26:06 +0200

    Users List - React + Redux

---
+++ lesson4-react-redux/task2/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import store from './store';
+import Users from './Users';
+
+const App = () => (
+  <Provider store={store}>
+    <Users />
+  </Provider>
+);
+
+export default App;
