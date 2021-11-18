3f49953b9cf3e9c7f3f92a941c0492753c45063d
annaKulyk <ankakulik1998@gmail.com>
2021-11-18 17:43:37 +0200

    Redux loger middleware - part 1

---
+++ lesson6-async-actions/task1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import Counter from './Counter';
+import store from './store';
+
+const App = () => (
+  <Provider store={store}>
+    <Counter />
+  </Provider>
+);
+
+export default App;
