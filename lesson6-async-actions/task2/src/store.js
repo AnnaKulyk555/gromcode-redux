da58c55f06a123c88f1b6f9ce7879a3050f4f51e
annaKulyk <ankakulik1998@gmail.com>
2021-11-22 11:06:56 +0200

    added logger middleware

--- lesson6-async-actions/task2/src/store.js
+++ lesson6-async-actions/task2/src/store.js
@@ -9,6 +9,16 @@ const reducer = combineReducers({
   users: usersReducer,
 });
 
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+const logger = store => next => action => {
+  console.group(action.type);
+  console.info('dispatching', action);
+  let result = next(action);
+  console.log('next state', store.getState());
+  console.groupEnd();
+
+  return result;
+};
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk, logger)));
 
 export default store;
